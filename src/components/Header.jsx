4173743afe8252918cac7a856b2e7a16828e2162
Kevin Figuera <kevinalejandro3@gmail.com>
2022-07-31 16:23:18 -0400

    useEffect: olvida el ciclo de vida, ahora piensa en efectos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,20 @@
-import {useState} from "react";
+import React, { useState } from 'react';
 
 const Header = () => {
-    const MIN_VALUE = 0;
-    const MAX_VALUE = 10;
-    const [darkMode, setDarkMode] = useState(false);
-    const [count, setCount] = useState(0);
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-    }
-    const increment = () => {
-        if(count < MAX_VALUE) {
-            setCount(count + 1);
-        } else {
-            setCount(MAX_VALUE);
-        }
-    }
-    const decrement = () => {
-        if(count > MIN_VALUE) {
-            setCount(count - 1);
-        } else {
-            setCount(MIN_VALUE);
-        }
-    }
-    return (
-        <header className="Header">
-            <h1>ReckHooks</h1>
-            <button type="button" onClick={toggleDarkMode}>{ darkMode ? 'Dark Mode' : 'Light Mode'}</button>
-            <div>
-                <button type="button" onClick={decrement}>-</button>{count}<button type="button" onClick={increment}>+</button>
-            </div>
+  const [darkMode, setDarkMode] = useState(false);
 
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  }
 
-        </header>
-    );
+  return (
+    <div className="Header">
+      <h1>ReactHooks</h1>
+      <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Light Mode'}</button>
+      <button type="button" onClick={() => setDarkMode
+        (!darkMode)}>{darkMode ? 'Dark Mode 2' : 'Light Mode 2'}</button>
+    </div>
+  );
 }
 
 export default Header;
